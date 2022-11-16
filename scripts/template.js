9dbdf9f440a07a9ba7034203a84639145ea912da
Ibrahim <ibrahimbahraj@gmail.com>
2022-11-16 08:07:54 -0500

    Third Phase

--- scripts/template.js
+++ scripts/template.js
@@ -1,29 +1,4 @@
 "use strict";
 
 
-// playground
-//     let nationalParkDataOutPut = document.getElementById(
-//         //     "nationalParkDataOutPut"
-//         //   );
-//         // for (let n of nationalParksArray) {
-//         //   let option = document.createElement("option");
-//         //   option.text = n.LocationName;
-//         //   nationalParkDataOutPut.appendChild(option);
-//         // }
-    
-
-
-//    }
-
-// selectMountain.onchange = () => {
-
-
-
-// }
-
-// for (let i = 0; i < mountainsArrayLength ; i++){
-//     let createdOption3 = document.createElement("option");
-//     createdOption3.text = mountainsArray[i];
-//     selectMountain.appendChild(createdOption3);
-//   }
 
